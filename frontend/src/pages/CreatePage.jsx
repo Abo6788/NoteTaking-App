94b4b13a6b6a40dc4c3cdb08053f0c86fbeca970
Abo6788 <abo.njoks@gmail.com>
2025-10-19 16:49:23 +0200

    working routes

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePage = () => {
   return (
     <div>
-      
+      <h1>CREATE PAGE</h1>
     </div>
   )
 }
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NoteDetailPage = () => {
   return (
     <div>
-      
+      <h1>NOTE DETAIL PAGE</h1>
     </div>
   )
 }
