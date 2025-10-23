3c09d157381749a875611cc0793e10935b0d4b10
Abo6788 <abo.njoks@gmail.com>
2025-10-23 17:32:28 +0200

    notes are being displayed on the hamepage - no design yet

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -32,6 +32,18 @@ const HomePage = () => {
   return (
     <div className='min-h-screen'>
         <NavBar/>
+        <div className="max-w-7xl mx-auto p-4 mt-6">
+          {loading && <div className="text-center text-primary py-10">Loading notes...</div>} 
+
+          {notes.length > 0 && (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {notes.map(note => (<div>
+                    {note.title} | {note.content}
+                </div>
+              ))}
+            </div>
+          )} 
+        </div>
     </div>
   )
 }
